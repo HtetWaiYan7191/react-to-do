1c9bc72f706b41bc53d3693d5bdb11cbedece9a1
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-05 17:44:20 +0630

    Add the buttons and inputs for edits

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import styles from '../styles/TodoItem.module.css';
+import React, { useState } from "react";
+import styles from "../styles/TodoItem.module.css";
 const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+  const [editing, setEditing] = useState(false);
 
-    return (
-      <li className={styles.item}>
-        <div className={styles.content}>
-        <input type="checkbox"
-        checked={itemProp.completed}
-        onChange={() => handleChange(itemProp.id)} />
-        <button type='button' onClick={() => delTodo(itemProp.id)}>Delete</button>
-        {itemProp.title}
-        </div>
+  let viewMode = {};
+  let editMode = {};
 
-      </li>
-    );
+  editing ? viewMode.display = "none " : editMode.display = "none"
+
+  const handleEditing = () => {
+    setEditing(true);
   };
-  
+  return (
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button type="button" onClick={() => delTodo(itemProp.id)}>
+          Delete
+        </button>
+        <button type="button" onClick={handleEditing}>
+          Edit
+        </button>
+        {itemProp.title}
+      </div>
+      <input type="text" className={styles.textInput} value={itemProp.title} style={editMode} />
+    </li>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
